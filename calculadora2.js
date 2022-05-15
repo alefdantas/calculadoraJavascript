a7c842c7f5614843fc5ca3343b996568cbb16135
alefdantas <sraleffps@hotmail.com>
2022-05-14 23:57:15 -0300

    terminando o processo do front-end da calculadora

---
+++ calculadora2.js
@@ -0,0 +1,14 @@
+var  n1=document.getElementById("n1").value;
+var  n2=document.getElementById("n2").value;
+var  n3=document.getElementById("n3").value;
+var  n4=document.getElementById("n4").value;
+var  n5=document.getElementById("n5").value;
+var  n6=document.getElementById("n6").value;
+var  n7=document.getElementById("n7").value;
+var  n8=document.getElementById("n8").value;
+var  n9=document.getElementById("n9").value;
+
+function N1(){
+    var  n0=document.getElementById("n0").value;
+    
+}
