b21ca8e0ab8f96d551985ae318d1316f9c0d3a93
alefdantas <sraleffps@hotmail.com>
2022-05-19 09:29:10 -0300

    adicionando funcionalidades no javascript

--- calculadora2.js
+++ calculadora2.js
@@ -1,14 +1,17 @@
-var  n1=document.getElementById("n1").value;
-var  n2=document.getElementById("n2").value;
-var  n3=document.getElementById("n3").value;
-var  n4=document.getElementById("n4").value;
-var  n5=document.getElementById("n5").value;
-var  n6=document.getElementById("n6").value;
-var  n7=document.getElementById("n7").value;
-var  n8=document.getElementById("n8").value;
-var  n9=document.getElementById("n9").value;
 
-function N1(){
-    var  n0=document.getElementById("n0").value;
-    
+
+function imprimir_algo(val){
+    var visor = document.getElementById("resultado");
+    visor.innerHTML = visor.innerHTML + val;
+   
+}
+function calcular(){
+    var visor = document.getElementById("resultado");
+    visor.innerHTML = eval(visor.innerHTML);
+   
+}
+function apagar(){
+    var visor = document.getElementById("resultado");
+    visor.innerHTML = "";
+   
 }
